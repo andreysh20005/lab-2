dc2d6208c4c4c06656e11761a91fa24008f88520
andreysh20005 <andrejsemakin838@gmail.com>
2026-03-03 20:52:32 +0500

    first commit

---
+++ lab 2 F#/Program.fs
@@ -0,0 +1,34 @@
+﻿open System
+
+let add_char char str :string=
+    str+char
+
+[<EntryPoint>]
+let main _ =
+    printf "введите количество элементов списка:"
+    
+    let flag, n  = Int32.TryParse(Console.ReadLine())
+
+    if flag then
+        if n<=0 then 
+            printf "число не является натуральным!"
+        else
+            let my_list = 
+                [
+                    for i in 1..n do
+                        printfn "введите строку:"
+                        yield Console.ReadLine()
+                ]
+            let rec rec_input () =
+                printfn "введите символ: "
+                let input = Console.ReadLine()
+                if input.Length = 1 then 
+                    input
+                else
+                    printfn "1 символ!"
+                    rec_input ()
+            let char = rec_input ()
+            printf "%A" (List.map (add_char char ) my_list)
+    else
+        printf "число не является целым"
+    0
---
+++ задание 2/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+
+let min (a: string) (b: string) = 
+    if a.Length < b.Length then
+        a
+    else
+        b
+
+[<EntryPoint>]
+let main _ =
+    printf "введите количество элементов списка:"
+    
+    let flag, n  = Int32.TryParse(Console.ReadLine())
+
+    if flag then
+        if n<=0 then 
+            printf "число не является натуральным!"
+        else
+            let my_list = 
+                [
+                    for i in 1..n do
+                        printfn "введите строку:"
+                        yield Console.ReadLine()
+                ]
+            
+            printf "минимальная длинна строки списка = %A" (List.fold min my_list.Head my_list.Tail).Length
+    else
+        printf "число не является целым"
+    0
