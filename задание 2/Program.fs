b4a46e5b08a1ce53e12ff1212517ab67efe8ca43
andreysh20005 <andrejsemakin838@gmail.com>
2026-03-06 12:39:33 +0500

    Update Program.fs, change codestyle

--- задание 2/Program.fs
+++ задание 2/Program.fs
@@ -1,29 +1,37 @@
-﻿open System
+open System
 
-let min (a: string) (b: string) = 
+let MinimumLenth (a: string) (b: string) = 
     if a.Length < b.Length then
         a
     else
         b
 
-[<EntryPoint>]
-let main _ =
-    printf "введите количество элементов списка:"
-    
-    let flag, n  = Int32.TryParse(Console.ReadLine())
+
+let rec InputNum () = 
+    printfn "введите количество элементов списка:"
+    let flag, n = Int32.TryParse(Console.ReadLine())
 
     if flag then
-        if n<=0 then 
-            printf "число не является натуральным!"
-        else
-            let my_list = 
-                [
-                    for i in 1..n do
-                        printfn "введите строку:"
-                        yield Console.ReadLine()
-                ]
-            
-            printf "минимальная длинна строки списка = %A" (List.fold min my_list.Head my_list.Tail).Length
+        if n <= 0 then 
+            printfn "число не является натуральным!"
+            InputNum ()
+        else n
     else
-        printf "число не является целым"
+        printfn "не является целым числом!"
+        InputNum ()
+        
+
+
+[<EntryPoint>]
+let main _ =
+
+    let n = InputNum () 
+    let MyList = 
+        [
+            for i in 1..n do
+                printfn "введите строку:"
+                yield Console.ReadLine()
+        ]  
+    let MinStrLen = (List.fold MinimumLenth MyList.Head MyList.Tail).Length
+    printf "минимальная длинна строки списка = %d" MinStrLen
     0
