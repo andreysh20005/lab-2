4ee04637e6d825d4b07030e4816a4db0d90159a1
andreysh20005 <andrejsemakin838@gmail.com>
2026-03-06 12:37:48 +0500

    Update Program.fs, change codestyle

--- lab 2 F#/Program.fs
+++ lab 2 F#/Program.fs
@@ -1,34 +1,43 @@
-﻿open System
+open System
 
-let add_char char str :string=
+let AddChar char str :string=
     str+char
+
+let rec RecInput () =
+    printfn "введите символ: "
+    let input = Console.ReadLine()
+    if input.Length = 1 then 
+        input
+    else
+        printfn "1 символ!"
+        RecInput ()
+
+
+let rec InputNum () = 
+    printfn "введите количество элементов списка:"
+    let flag, n = Int32.TryParse(Console.ReadLine())
+
+    if flag then
+        if n <= 0 then 
+            printfn "число не является натуральным!"
+            InputNum ()
+        else n
+    else
+        printfn "не является целым числом!"
+        InputNum ()
+
 
 [<EntryPoint>]
 let main _ =
-    printf "введите количество элементов списка:"
-    
-    let flag, n  = Int32.TryParse(Console.ReadLine())
+    let n = InputNum ()
+    let MyList = 
+        [
+            for i in 1..n do
+                printfn "введите строку:"
+                yield Console.ReadLine()
+        ]
 
-    if flag then
-        if n<=0 then 
-            printf "число не является натуральным!"
-        else
-            let my_list = 
-                [
-                    for i in 1..n do
-                        printfn "введите строку:"
-                        yield Console.ReadLine()
-                ]
-            let rec rec_input () =
-                printfn "введите символ: "
-                let input = Console.ReadLine()
-                if input.Length = 1 then 
-                    input
-                else
-                    printfn "1 символ!"
-                    rec_input ()
-            let char = rec_input ()
-            printf "%A" (List.map (add_char char ) my_list)
-    else
-        printf "число не является целым"
+    let char = RecInput ()
+    let new_list = (List.map (AddChar char ) MyList)
+    printf "Итоговый результат: %A" new_list
     0
